dfe68118ff8e88423e24ead4cccba31b14245393
Sebastiaan Visser <sfvisser@cs.uu.nl>
2009-05-07 17:28:48 +0200

    Modules with dependency test.

---
+++ tests/Dep.hs
@@ -0,0 +1,4 @@
+module Dep where
+
+test :: [(Char, Integer)]
+test = undefined $ zip "Monkey!" [1..]
---
+++ tests/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import Dep
+
+main :: IO ()
+main = putStrLn $ map undefined test
+
