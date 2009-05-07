ae2f2f4bcb2a48a1420fd674c3360e849c1b8d40
Erik Hesselink <hesselink@gmail.com>
2009-05-07 18:56:21 +0200

    Revert "In-memory linking. No files are being created on the FS." since it somehow reverted a few previous changes.
    This reverts commit b5f0be5f37f0e948455b21a877afdb8c32f0b113.

--- src/Development/GhcGoals.hs
+++ src/Development/GhcGoals.hs
@@ -4,11 +4,15 @@
 module Development.GhcGoals (
       goals
     , goalsWith
+    , getGoals
+    , getGoalsWith
     , pprGoals
-    , pprTypeSpecForUser
     ) where
 
-import GHC (defaultErrorHandler, load, ghcLink, GhcLink(..), hscTarget, HscTarget(..), LoadHowMuch(..), runGhc, getSessionDynFlags, setSessionDynFlags, guessTarget, setTargets, depanal, parseModule, typecheckModule, dopt, DynFlag(Opt_PrintExplicitForalls))
+import Control.Monad (liftM)
+import Data.List (sortBy)
+
+import GHC (defaultErrorHandler, load, LoadHowMuch(..), runGhc, getSessionDynFlags, setSessionDynFlags, guessTarget, setTargets, depanal, parseModule, typecheckModule, dopt, DynFlag(Opt_PrintExplicitForalls), SuccessFlag(..), handleSourceError, printExceptionAndWarnings)
 import GHC.Paths (libdir)
 import DynFlags (defaultDynFlags)
 import MonadUtils (liftIO)
@@ -18,28 +22,41 @@ import PprTyThing (pprTypeForUser)
 
 import Development.GhcGoals.Collector
 
--- | Analyze a file, returning type information for all 'undefined's.
-goals :: FilePath -> IO [GoalInfo]
+-- | Analyze a file, print type information for all 'undefined's
+goals :: FilePath -> IO ()
 goals = goalsWith ["undefined"]
+
+-- | Analyze a file, print type information for all variables with the
+-- specified names.
+goalsWith :: [String] -> FilePath -> IO ()
+goalsWith goals file = getGoalsWith goals file >>= pprGoals
+
+-- | Analyze a file, returning type information for all 'undefined's.
+getGoals :: FilePath -> IO [GoalInfo]
+getGoals = getGoalsWith ["undefined"]
 
 -- | Analyze a file, returning type information for all variables with
 -- the specified names.
-goalsWith :: [String] -> FilePath -> IO [GoalInfo]
-goalsWith goals file =
-    defaultErrorHandler defaultDynFlags $
-      runGhc (Just libdir) $ do
-        dflags   <- getSessionDynFlags
-        setSessionDynFlags ( dflags
-          { ghcLink   = LinkInMemory
-          , hscTarget = HscNothing -- Interpreted
-          })
-        target   <- guessTarget file Nothing
-        setTargets [target]
-        load LoadAllTargets
-        (md:mds) <- depanal [] True
-        pm       <- parseModule md
-        tcm      <- typecheckModule pm
-        return $ goalsFor tcm goals
+getGoalsWith :: [String] -> FilePath -> IO [GoalInfo]
+getGoalsWith goals file = liftM (sortBy compareGoalInfo) gs
+  where
+    gs = defaultErrorHandler defaultDynFlags $
+           runGhc (Just libdir) $ handleSourceError (\x -> printExceptionAndWarnings x >> return []) $ do
+             dflags   <- getSessionDynFlags
+             setSessionDynFlags dflags
+             target   <- guessTarget file Nothing
+             setTargets [target]
+             success <- load LoadAllTargets
+             case success of
+               Succeeded -> do
+                 (md:mds) <- depanal [] True
+                 pm       <- parseModule md
+                 tcm      <- typecheckModule pm
+                 return $ goalsFor tcm goals
+               Failed     -> return []
+
+compareGoalInfo :: GoalInfo -> GoalInfo -> Ordering
+compareGoalInfo (_, s1, _) (_, s2, _) = compare s1 s2
 
 -- | Pretty print information on goals in a style similar to GHCi.
 pprGoals :: [GoalInfo] -> IO ()
